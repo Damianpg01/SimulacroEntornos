ade61f66bb85747dd38da5e8ad6611f1f76317d1
damianpergon01-admin <dpgpunta01@gmail.com>
2022-11-29 21:16:43 +0100

    Primer Commit

---
+++ Simulacro Entornos/src/Simulacro/Main.java
@@ -0,0 +1,21 @@
+/**
+ * 
+ */
+package Simulacro;
+
+/**
+ * @author Damian
+ *
+ */
+public class Main {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hola Mundo");
+
+	}
+
+}
